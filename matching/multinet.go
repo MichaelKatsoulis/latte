f0416dbec4ad05cf17786a3ac2f1cae40dadcaf5
Nikos Anastopoulos <n.anastop@gmail.com>
2016-08-25 16:04:17 +0300

    Refactor bounds variables

--- matching/multinet.go
+++ matching/multinet.go
@@ -29,26 +29,26 @@ var MultinetMatch = Match{
 // If the pattern is found it is returned. Otherwise nil is returned.
 func MultinetPktInCheck(ofPkt []byte, ip net.IP, port uint16) []byte {
 	var pattern, r []byte
-	var from, to int
+	var i, j int
 
-	from = ofp14.PKTIN_OFPMATCH_OFFSET + ofp14.OFPMATCH_LENGTH_OFFSET
-	to = from + ofp14.OFPMATCH_LENGTH_BYTES
-	matchlen := int(binary.BigEndian.Uint16(ofPkt[from:to]))
+	i = ofp14.PKTIN_OFPMATCH_OFFSET + ofp14.OFPMATCH_LENGTH_OFFSET
+	j = i + ofp14.OFPMATCH_LENGTH_BYTES
+	matchlen := int(binary.BigEndian.Uint16(ofPkt[i:j]))
 	// "ofp_match is padded as needed, to make its overall size a multiple of 8"
 	matchlen = roundUp(matchlen, 3)
 	// compute data index
 	di := ofp14.PKTIN_OFPMATCH_OFFSET + matchlen + ofp14.PKTIN_PAD_BYTES
 	data := ofPkt[di:]
 	// extract data ethertype
-	from = ofp14.ETHER_ETHERTYPE_OFFSET
-	to = ofp14.ETHER_DATA_OFFSET
-	etherType := int(binary.BigEndian.Uint16(data[from:to]))
+	i = ofp14.ETHER_ETHERTYPE_OFFSET
+	j = ofp14.ETHER_DATA_OFFSET
+	etherType := int(binary.BigEndian.Uint16(data[i:j]))
 	// ARP
 	if etherType == 0x0806 {
 		//dstMac: data[0:6], srcMac: data[6:12]
-		from = ofp14.ETHER_DSTMAC_OFFSET
-		to = ofp14.ETHER_ETHERTYPE_OFFSET
-		pattern = data[from:to]
+		i = ofp14.ETHER_DSTMAC_OFFSET
+		j = ofp14.ETHER_ETHERTYPE_OFFSET
+		pattern = data[i:j]
 	}
 	if pattern == nil {
 		r = nil
@@ -80,16 +80,16 @@ func MultinetPktInCheck(ofPkt []byte, ip net.IP, port uint16) []byte {
 // - Receiver (OF switch) port (given as argument)
 func MultinetFlowModCheck(ofPkt []byte, ip net.IP, port uint16) []byte {
 	var matchDstMac, matchSrcMac []byte
-	var from, to int
-	from = ofp14.FLOWMOD_MATCH_OXM0_OFFSET
-	to = ofp14.FLOWMOD_MATCH_OXM0_OFFSET + ofp14.OXM_FIELD_OFFSET
-	oxm0Class := int(binary.BigEndian.Uint16(ofPkt[from:to]))
+	var i, j int
+	i = ofp14.FLOWMOD_MATCH_OXM0_OFFSET
+	j = ofp14.FLOWMOD_MATCH_OXM0_OFFSET + ofp14.OXM_FIELD_OFFSET
+	oxm0Class := int(binary.BigEndian.Uint16(ofPkt[i:j]))
 
 	if oxm0Class == ofp14.OFPXMC_OPENFLOW_BASIC {
-		from = ofp14.FLOWMOD_MATCH_OXM0_OFFSET + ofp14.OXM_FIELD_OFFSET
-		oxm0Field := uint8(ofPkt[from])
-		from = ofp14.FLOWMOD_MATCH_OXM0_OFFSET + ofp14.OXM_LENGTH_OFFSET
-		oxm0ValLength := uint8(ofPkt[from])
+		i = ofp14.FLOWMOD_MATCH_OXM0_OFFSET + ofp14.OXM_FIELD_OFFSET
+		oxm0Field := uint8(ofPkt[i])
+		i = ofp14.FLOWMOD_MATCH_OXM0_OFFSET + ofp14.OXM_LENGTH_OFFSET
+		oxm0ValLength := uint8(ofPkt[i])
 		oxm0ValFrom := ofp14.FLOWMOD_MATCH_OXM0_OFFSET + ofp14.OXM_VALUE_OFFSET
 		oxm0ValTo := oxm0ValFrom + int(oxm0ValLength)
 
